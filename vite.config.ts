fc2783c7d1020d84ded65b3bd809c79432036e22
unknown <ashrafkareem2004@gmail.com>
2025-03-02 17:20:36 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/React_Ecommerce/",
+  base: "./",
   build: {
     outDir: 'dist', // Make sure this line exists
   },
